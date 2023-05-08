134b8e38f9cbdf870821b38a75449b4098f33276
SantyPrado01 <santiago01prado@gmail.com>
2023-05-08 13:12:14 -0300

    Tuplas2

--- Tuplas/tuplas.py
+++ Tuplas/tuplas.py
@@ -1,6 +1,6 @@
 #Precio Maximo 120
-precioMax = 100
-marca = 'Nike'
+precioMax = 120
+marca = 'Adidas'
 lista = [('nombrepProducto', 100, 2, 'Nike'),('nombreProducto', 120, 2, 'Nike'),('nombreProducto', 110, 2, 'Adidas')]
 lista2= []
 for i in lista:
